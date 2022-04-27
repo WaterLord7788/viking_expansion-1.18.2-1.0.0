989f931248e7b300f297cd19ffe448bbe6e3eb6e
WaterLord7788 <izympyd.master@gmail.com>
2022-04-27 05:02:28 -0700

    Dowsing rod was added Dowsing rod will search for valuable ores underground.
    Kristian, 17:02 27/04/2022

--- src/main/java/com/waterlord7788/viking_expansion/items/ModItems.java
+++ src/main/java/com/waterlord7788/viking_expansion/items/ModItems.java
@@ -1,6 +1,7 @@
 package com.waterlord7788.viking_expansion.items;
 
 import com.waterlord7788.viking_expansion.VikingExpansion;
+import com.waterlord7788.viking_expansion.items.custom.DowsingRodItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.VIKINGEXPANSION_TAB)));
+
+    public static final RegistryObject<Item> DOWSING_ROD = ITEMS.register("dowsing_rod",
+            () -> new DowsingRodItem(new Item.Properties().tab(ModCreativeModeTab.VIKINGEXPANSION_TAB).durability(16)));
+
 
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
