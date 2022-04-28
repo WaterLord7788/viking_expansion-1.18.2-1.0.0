397fa59ce8f4483621afc61806ad8d31ffd27267
WaterLord7788 <izympyd.master@gmail.com>
2022-04-28 06:55:38 -0700

    Textures Changes Changed textures for raw sapphire and sapphire. New textures are re-designed and they look fresh!
    Kristian, 18:55 28/04/2022

--- src/main/java/com/waterlord7788/viking_expansion/VikingExpansion.java
+++ src/main/java/com/waterlord7788/viking_expansion/VikingExpansion.java
@@ -44,6 +44,6 @@ public class VikingExpansion {
     private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        LOGGER.info("DIRT BLOCK >> ${\\}", Blocks.DIRT.getRegistryName());
     }
 }
