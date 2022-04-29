3aae75469f8927e4d7d3dbdcd4a7c75606531f45
WaterLord7788 <izympyd.master@gmail.com>
2022-04-28 21:15:50 -0700

    Tooltips Added a tooltip for dowsing rod.
    Kristian, 9:15 29/04/2022

--- src/main/java/com/waterlord7788/viking_expansion/blocks/ModBlocks.java
+++ src/main/java/com/waterlord7788/viking_expansion/blocks/ModBlocks.java
@@ -4,10 +4,11 @@ import com.waterlord7788.viking_expansion.VikingExpansion;
 import com.waterlord7788.viking_expansion.blocks.custom.SpeedyBlock;
 import com.waterlord7788.viking_expansion.items.ModCreativeModeTab;
 import com.waterlord7788.viking_expansion.items.ModItems;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.ColorResolver;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
@@ -15,8 +16,10 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.Nullable;
 import org.lwjgl.system.CallbackI;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -49,7 +52,23 @@ public class ModBlocks {
                     .requiresCorrectToolForDrops()), ModCreativeModeTab.VIKINGEXPANSION_TAB);
 
 
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab, String tooltipKey){
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, tab, tooltipKey);
+        return toReturn;
+    }
 
+    // If you want to add tooltip for a block, pass a string for example `tooltip.vikingexpansion.block.speedy_block` and translate it in the en_us.json
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
+                                                                           CreativeModeTab tab, String tooltipKey){
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
+                new Item.Properties().tab(tab)){
+            @Override
+            public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
+                pTooltip.add(new TranslatableComponent(tooltipKey));
+            }
+        });
+    }
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
